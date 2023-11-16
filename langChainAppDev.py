41f687d29a551ff5b270f9d000063a9194da79c5
Sheikh Najam <64066796+nds-najam@users.noreply.github.com>
2023-11-16 06:19:56 +0530

    Update langChainAppDev.py

--- langChainAppDev.py
+++ langChainAppDev.py
@@ -1,1 +1,32 @@
+#!pip install python-dotenv
+#!pip install openai
 
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# account for deprecation of LLM model
+import datetime
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+def get_completion(prompt, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, 
+    )
+    return response.choices[0].message["content"]
